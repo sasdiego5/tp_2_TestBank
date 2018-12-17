25343dc6142d73dfa10e83ff228cfe1f3d175eaa
June <June@LAPTOP-O1SHBO3I>
2018-12-17 01:08:51 -0600

    Change with Grading

--- tp_2_TestBank/src/grading.java
+++ tp_2_TestBank/src/grading.java
@@ -1,32 +1,62 @@
+import java.time.chrono.ThaiBuddhistDate;
+import java.util.Scanner;
+
+import org.junit.experimental.theories.Theories;
+
 import edu.princeton.cs.introcs.StdIn;
 import edu.princeton.cs.introcs.StdOut;
 
 public class grading {
-	
+	public Student student;
 	
 	static String[]  answer = {"b","a","d"};
-	static String[]  Stu_Answer = new String[3];
+	static String[]  Stu_Answer1 = new String[3];
+	
+	public static int grade = 0;
+	   
+    public static int getGrade() {
+		return grade;
+	}
+	 
+    public String[] getStu_Answer() {
+		return this.Stu_Answer1;
+	}
     public static void main(String[] args) {
-    	StdOut.println("Please input the first question answer");
-	    String first = StdIn.readLine(); 
-	    Stu_Answer[0] = first;
+    	String [] Stu_Answer = getanswer();
+    	GetGrade(Stu_Answer);
+	}
+
+
+
+	public static String[] getanswer() {
+		Scanner console = new Scanner(System.in);
+		StdOut.println("Please input the first question answer");
+		String first = console.nextLine();
+		Stu_Answer1[0] = first;
 		StdOut.println("Please input the Second question answer");
-	    String Second = StdIn.readLine(); 
-	    Stu_Answer[1] = Second;	
+	    String Second =console.nextLine();
+	    Stu_Answer1[1] = Second;	
 	    StdOut.println("Please input the first question answer");
-	    String Third = StdIn.readLine(); 
-	    Stu_Answer[2] = Third;
-	    
-	    int grade = 0;
+	    String Third = console.nextLine();
+	    Stu_Answer1[2] = Third;
+		
+	    return Stu_Answer1;
+		
+	}
+   
+
+	public static int GetGrade(String[] Stu_Answer) {
+	    Scanner console = new Scanner(System.in);
+		
+       
 	   for(int i=0; i<answer.length; i++) {
 		   if(Stu_Answer[i].equals(answer[i])) {
 			   grade+=1;
-		   }
-	   }
-	   
-	   
-	   
-	   StdOut.print("Your Grade is: " + grade + " out of 3");
-
-	}
+		   } 	
+    }
+	   //StdOut.println("the grade is: "+grade);
+	   return grade;
+    
+    
+}
 }
